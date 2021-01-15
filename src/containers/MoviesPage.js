c0ab688f3a9bb841b0996cb141417ad35ea56f94
layla  hammoud <laylahammoud63@gmail.com>
2021-01-15 08:50:58 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -4,9 +4,9 @@ import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
 // import the `MovieShow` component:
 import MovieShow from '../components/MovieShow';
- 
-// Here we add `match` to the arguments so we can access the path information 
-// in `routerProps` that is passed from App.js 
+
+// Here we add `match` to the arguments so we can access the path information
+// in `routerProps` that is passed from App.js
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
@@ -15,5 +15,5 @@ const MoviesPage = ({ match, movies }) => (
     <Route path={`${match.url}/:movieId`} component={MovieShow}/>
   </div>
 )
- 
+
 export default MoviesPage
